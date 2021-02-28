fef43796a2047585d70f07a8ef8c467cc59784ea
unknown <matiasmuhn@gmail.com>
2021-02-28 19:51:29 -0300

    testeando

--- docs/js/countdown.js
+++ docs/js/countdown.js
@@ -3,7 +3,7 @@ simplyCountdown('#cuenta', {
 	month: 2, // required
 	day: 28, // required
 	hours: 16, // Default is 0 [0-23] integer
-	minutes: 51, // Default is 0 [0-59] integer
+	minutes: 55, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
